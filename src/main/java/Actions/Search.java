c6c1f666771b23b3d3e5744986b183f2899fa920
Demitsuru <byelov.dmitriy@gmail.com>
2016-03-16 15:21:47 +0200

    BaseTest implemented Screenshotter in @AfterMethod. Removed String search method. Corrected XML property loader

--- src/main/java/Actions/Search.java
+++ src/main/java/Actions/Search.java
@@ -5,26 +5,12 @@ import PageObjects.SecondSearchPage;
 import java.util.List;
 import java.util.Properties;
 
-import static Utils.PropertiesLoader.propertyXMLoader;
-
 /**
  * Created by DeBeers on 13.03.2016.
  */
 public class Search {
 
-    public static List<String> search(SecondSearchPage secondSearchPage, String cityIndex, String categoryIndex,
-                               String undercatIndex, String searchRequest) throws InterruptedException {
-
-        secondSearchPage.selectCityForSearch(cityIndex);
-        secondSearchPage.selectCategorie(categoryIndex);
-        secondSearchPage.selectUnderCategorie(undercatIndex);
-        secondSearchPage.enterSearchRequest(searchRequest);
-        secondSearchPage.clickOnSeaqrchButton();
-
-        return secondSearchPage.foundEqualsTitlesInSearchResults(searchRequest);
-    }
-
-    public static List<String> search(SecondSearchPage secondSearchPage,Properties props) throws InterruptedException {
+    public static List<String> search(SecondSearchPage secondSearchPage, Properties props) throws InterruptedException {
 
         secondSearchPage.selectCityForSearch(props.getProperty("City"));
         secondSearchPage.selectCategorie(props.getProperty("Category"));
