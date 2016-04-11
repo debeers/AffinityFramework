46ea75faa086b196b037f886d51dccf610667060
Demitsuru <byelov.dmitriy@gmail.com>
2016-04-11 17:24:33 +0300

    ArrayList

--- src/main/java/Tests/BaseTest.java
+++ src/main/java/Tests/BaseTest.java
@@ -4,12 +4,17 @@ import DBUtils.DBConnection;
 import Entities.LoginObject;
 import GeneralHelpers.Logger;
 import com.codeborne.selenide.WebDriverRunner;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.client.ClientUtil;
+import net.lightbody.bmp.core.har.Har;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.LoggerFactory;
@@ -19,13 +24,11 @@ import org.testng.annotations.*;
 import ru.stqa.selenium.factory.WebDriverFactory;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Calendar;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import static UtilsGUI.PropertiesLoader.propertyXMLoader;
 
 
 public class BaseTest {
@@ -37,6 +40,8 @@ public class BaseTest {
     public static LoginObject clientLogin;
     public static org.slf4j.Logger log;
     public static java.sql.Connection jdbcConnection;
+    public static BrowserMobProxyServer server;
+    public static ClientUtil seleniumProxy;
 
 
     @Parameters({"DB_DRIVER", "DB_CONNECTION", "DB_USER", "DB_PASSWORD"})
@@ -46,6 +51,9 @@ public class BaseTest {
 
         jdbcConnection = new DBConnection().initDBConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
         Registry.Registry.set("dbConnection", jdbcConnection);
+        this.server = new BrowserMobProxyServer();
+        server.start(0);
+
     }
 
     @Parameters({"URL", "clientLoginParam", "clientPasswordParam"})
@@ -56,10 +64,11 @@ public class BaseTest {
         String TestClassName = this.getClass().getName();
         System.out.println(TestClassName);
 
-        clientLogin      = new LoginObject(clientLoginParam, clientPasswordParam);
-        baseUrl          = URL;
-        log              = LoggerFactory.getLogger(Logger.class);
+        clientLogin = new LoginObject(clientLoginParam, clientPasswordParam);
+        baseUrl = URL;
+        log = LoggerFactory.getLogger(Logger.class);
 
+        Proxy seleniumProxy = ClientUtil.createSeleniumProxy(server);
         String path = System.getProperty("user.dir") + "\\src\\main\\java\\Downloaded_Files";
         File downloadDir = new File(path);
         FirefoxProfile fProfile = new FirefoxProfile();
@@ -71,6 +80,7 @@ public class BaseTest {
         fProfile.setPreference("browser.helperApps.neverAsk.saveToDisk", "text/plain");
 
         DesiredCapabilities dc = DesiredCapabilities.firefox();
+        dc.setCapability(CapabilityType.PROXY, seleniumProxy);
         dc.setJavascriptEnabled(true);
         dc.setCapability(FirefoxDriver.PROFILE, fProfile);
 
@@ -79,19 +89,20 @@ public class BaseTest {
         wait = new WebDriverWait(driver, 10);
         driver.manage().window().maximize();
         WebDriverRunner.setWebDriver(driver);
-
+        server.newHar("lalafo.az");
 
         try {
             driver.get(URL);
             Assert.assertTrue(driver.getCurrentUrl().contains(URL), "We are not on main page!"
                     + driver.getCurrentUrl() + "  " + "But expected:::: " + URL);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("<<<<< We are not on the MAIN PAGE >>>>>");
         }
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDownAfterTest(ITestResult result)throws Exception {
+    public void tearDownAfterTest(ITestResult result) throws Exception {
+
         if (ITestResult.FAILURE == result.getStatus()) {
             try {
                 // Create refernce of TakesScreenshot
@@ -141,4 +152,36 @@ public class BaseTest {
             Assert.fail(verificationErrorString);
         }
     }
+
+    @AfterSuite
+    public void terminate() throws Exception {
+        try {
+
+            Calendar calendar = Calendar.getInstance();
+            Har har = server.getHar();
+
+            File file = new File("./HarReports/" + "Test"
+                    + "_"
+                    + calendar.get(Calendar.MONTH)
+                    + "_"
+                    + calendar.get(Calendar.DAY_OF_MONTH)
+                    + "_"
+                    + calendar.get(Calendar.HOUR_OF_DAY)
+                    + "_"
+                    + calendar.get(Calendar.MINUTE)
+                    + "_"
+                    + calendar.get(Calendar.SECOND)
+                    + ".har");
+
+            FileOutputStream fos = new FileOutputStream(file);
+            try {
+                har.writeTo(fos);
+            } catch (IOException e) {
+                log.info("HOUSTON, WE HAVE A PROBLEM - INFO WAS NOT WRITTEN INTO HAR FILE:::" + file);
+            }
+        } catch (Exception e) {
+            log.info("HOUSTON, WE HAVE A PROBLEM - DEBUG PLEASE TO CHECK IF A HAR FILE WAS CREATED");
+        }
+    }
+
 }
